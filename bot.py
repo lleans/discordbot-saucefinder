c0e987320b91eedfb38b3245ab15c2cfcc434298
lleans <ahmadunnail@gmail.com>
2021-02-13 16:54:00 +0700

    new API, supported video

--- bot.py
+++ bot.py
@@ -1,53 +1,116 @@
 import discord
 import os
+import io
 import saucer
 import re
 import kadal
 
+from colorthief import ColorThief
+from urllib.request import urlopen, Request
+from itertools import islice
+
+
 class MaidOurdick(discord.Client):
     def __init__(self):
         super().__init__()
-        self.regex = re.compile(r'"(.*?)"')
+        self.regex_sauce_image = re.compile(r'"(.*?)"')
+        self.regex_sauce_video = re.compile(r"mp4")
         self.klient = kadal.Klient()
         print("start")
 
     async def on_ready(self):
         print('Logged as', maid.user.name, ",", maid.user.id)
 
-    async def format_embed(self, sauce):
+    async def format_embed(self, sauce, type):
+        if type == "manga":
+            try:
+                media = await self.klient.search_manga(sauce.title, popularity=True)
+                desc = "Likely **" + \
+                    str(sauce.similiar) + f"%**\n\n***" + \
+                    ", ".join(media.genres) + "***\n"
+                if media.description is not None:
+                    desc += media.description[:256 - len(
+                        desc)] + f"... [more]({media.site_url})\n\nAnother Results \n"
+                    desc = desc.replace("<br>", "").replace(
+                        "<i>", "").replace("</i>", "")
+                else:
+                    desc += "\n\nAnother Results \n"
+            except kadal.MediaNotFound:
+                desc = "Likely **" + str(sauce.similiar) + \
+                    f"%**\n\nAnother Results \n"
+        
+        if type == "anime":
+            try:
+                media = await self.klient.search_anime(sauce.title, popularity=True)
+                desc = "Likely **" + \
+                    str(sauce.similiar) + f"%**\n\n***" + \
+                    ", ".join(media.genres) + "***\n"
+                if media.description is not None:
+                    desc += media.description[:256 - len(
+                        desc)] + f"... [more]({media.site_url})\n\nAnother Results: \n"
+                    desc = desc.replace("<br>", "").replace(
+                        "<i>", "").replace("</i>", "")
+                else:
+                    desc += "\n\nAnother Results: \n"
+            except kadal.MediaNotFound:
+                desc = "Likely **" + str(sauce.similiar) + \
+                    f"%**\n\nAnother Results: \n"
+        for another in islice(sauce.another, 0, 5):
+            try:
+                desc += f"** • [{another.title[:150 - len(another.title)]}...]({another.url})**\n"
+            except:
+                pass
+            try:
+                desc += f"** • [{another.title[:150 - len(another.title)]}...]({another.thumbnail})**\n"
+            except:
+                pass
+            try:
+                desc += f"** • [{another.titles[:150 - len(another.titles)]}...]({another.urls})**\n"
+            except:
+                pass
+        
+        req = Request(sauce.thumbnail, headers={'User-Agent': 'Mozilla/5.0'})
+        fd = urlopen(req)
+        f = io.BytesIO(fd.read())
+        color_thief = ColorThief(f)
+        
+        e = discord.Embed(title=sauce.title, description=desc,
+                          color=discord.Color.from_rgb(color_thief.get_color(quality=1)[0], color_thief.get_color(quality=1)[1], color_thief.get_color(quality=1)[2]))
+        e.set_image(url=sauce.thumbnail)
         try:
-            media = await self.klient.search_manga(sauce[0].title, popularity=True)
-            desc = "Likely **" + str(sauce[0].similarity) + f"%**\n\n***" + ", ".join(media.genres) + "***\n"
-            if media.description is not None:
-                desc += media.description[:256 - len(desc)] + f"... [(more)]({media.site_url})\n\nAnother Results \n"
-                desc = desc.replace("<br>", "").replace("<i>", "").replace("</i>", "")
-            else:
-                desc += "\n\nAnother Results \n"
-        except kadal.MediaNotFound:
-            desc = "Likely **" + str(sauce[0].similarity) + f"%**\n\nAnother Results \n"
-        for another in sauce:
-            data = another.title
-            desc += f"**  • {data}**\n"
-        e = discord.Embed(title=sauce[0].title, description=desc, color=discord.Color.random())
-        e.set_image(url=sauce[0].thumbnail)
-        try:
-            e.url = sauce[0].urls[0]
+            e.url = sauce.url
         except IndexError:
             pass
-        e.set_footer(text="If the search result is below 50% then do a manual search")
+        e.set_footer(text=f"Source from {sauce.frm}")
         return e
 
     async def on_message(self, message):
-        if message.author == self.user:  # Ignore own messages
+        if message.author == self.user:
             return
-        m = self.regex.findall(message.clean_content)
+        m = self.regex_sauce_image.findall(message.clean_content)
         m_clean = list(filter(bool, m))
         if m_clean:
-            sauce = saucer.saucer_sauceNao(m_clean[0])
-            await message.channel.send(m_clean[0])
-            embed = await self.format_embed(sauce)
-            await message.channel.send(embed=embed)
+            vid = self.regex_sauce_video.findall(m_clean[0])    
+            if vid:
+                embedwait = await self.wait_please()
+                temp = await message.channel.send(embed=embedwait)
+                sauce = saucer.sauce(m_clean[0], type="anime")
+                embed = await self.format_embed(sauce, type="anime")
+                await temp.delete()
+                await message.channel.send(embed=embed)
+                await message.channel.send(sauce.url)
+            else:
+                embedwait = await self.wait_please()
+                temp = await message.channel.send(embed=embedwait)
+                sauce = saucer.sauce(m_clean[0], type="image")
+                embed = await self.format_embed(sauce, type="manga")
+                await temp.delete()
+                await message.channel.send(embed=embed)
 
+    async def wait_please(self):
+        a = discord.Embed(title="Searching the sauce ...", description="Wait a second plzz", color=discord.Color.from_rgb(255, 201, 107))
+        a.set_image(url="https://i.imgur.com/GE8uaS4.gif")
+        return a
 
 maid = MaidOurdick()
 f = open("TOKEN")
--- saucer.py
+++ saucer.py
@@ -1,8 +1,125 @@
-from saucenao_api import SauceNao
+import re
+from PicImageSearch import SauceNAO, TraceMoe, Ascii2D, Iqdb
+import logging
+from loguru import logger
 
-b = open("API_sauceNao")
-saucer = SauceNao(b.readline())
+logger = logging.getLogger('my-logger')
+logger.propagate = False
+
+
+class Sauce:
+    title = ""
+    similiar = ""
+    thumbnail = ""
+    url = ""
+    frm = ""
+    another = ""
+
+
+def sauce(name, type):
+    if type == "anime":
+        sauce_anime(name)
+
+    if type == "image":
+        sauce_image(name)
+
+    sauce = Sauce()
+
+    return sauce
+
+
+def sauce_anime(name):
+    tMoe = saucer_TraceMoe(name).raw[0]
+    tMoesimilar = float("{:.2f}".format(tMoe.similarity * 100))
+
+    setattr(Sauce, 'title', tMoe.title_english)
+    setattr(Sauce, 'similiar', tMoesimilar)
+    setattr(Sauce, 'thumbnail', tMoe.thumbnail)
+    setattr(Sauce, 'url', tMoe.video_thumbnail)
+    setattr(Sauce, 'frm', "Trace Moe")
+    setattr(Sauce, 'another', saucer_TraceMoe(name).raw)
+
+
+def sauce_image(name):
+    sNao = saucer_sauceNao(name).raw[0]
+    A2d = saucer_Ascii2D(name).raw[1]
+    try:
+        Iq = saucer_Iqdb(name).raw[0]
+        regexIq = re.compile("[0-9]+")
+        Iqs = regexIq.findall(Iq.similarity)
+        Iqsi_clean = list(filter(bool, Iqs))
+        Iqsimilar = float(Iqsi_clean[0])
+    except AttributeError:
+        Iqsimilar = float(0)
+
+    try:
+        Iq3d = saucer_Iqdb3d(name).raw[0]
+        regexIqs = re.compile("[0-9]+")
+        Iq3D = regexIqs.findall(Iq.similarity)
+        Iqs3D_clean = list(filter(bool, Iq3D))
+        Iq3dsimilar = float(Iqs3D_clean[0])
+    except AttributeError:
+        Iq3dsimilar = float(0)
+
+    sNaosimilar = float(sNao.similarity)
+
+    if sNaosimilar <= 50 and Iqsimilar <= 50 and Iq3dsimilar <= 50:
+        setattr(Sauce, 'title', A2d.titles[0][:150 - len(A2d.titles)] + "...")
+        setattr(Sauce, 'similiar', None)
+        setattr(Sauce, 'thumbnail', A2d.thumbnail[0])
+        setattr(Sauce, 'url', A2d.urls[0])
+        setattr(Sauce, 'frm', "Ascii2d")
+        setattr(Sauce, 'another', saucer_Ascii2D(name).raw)
+    elif sNaosimilar >= Iqsimilar and sNaosimilar >= Iq3dsimilar:
+        setattr(Sauce, 'title', sNao.title[:150 - len(sNao.title)] + "...")
+        setattr(Sauce, 'similiar', sNaosimilar)
+        setattr(Sauce, 'thumbnail', sNao.thumbnail)
+        setattr(Sauce, 'url', sNao.url)
+        setattr(Sauce, 'frm', "SauceNao")
+        setattr(Sauce, 'another', saucer_sauceNao(name).raw)
+    elif Iqsimilar >= sNaosimilar and Iqsimilar >= Iq3dsimilar:
+        setattr(Sauce, 'title', Iq.title[:150 - len(Iq.title)] + "...")
+        setattr(Sauce, 'similiar', Iqsimilar)
+        setattr(Sauce, 'thumbnail', Iq.thumbnail)
+        setattr(Sauce, 'url', f"https:{Iq.url}")
+        setattr(Sauce, 'frm', "Iqdb")
+        setattr(Sauce, 'another', saucer_Iqdb(name).raw)
+    elif Iq3dsimilar >= sNaosimilar and Iq3dsimilar >= Iqsimilar:
+        setattr(Sauce, 'title', Iq3d.title[:150 - len(Iq3d.title)] + "...")
+        setattr(Sauce, 'similiar', Iq3dsimilar)
+        setattr(Sauce, 'thumbnail', Iq3d.thumbnail)
+        setattr(Sauce, 'url', f"https:{Iq3d.url}")
+        setattr(Sauce, 'frm', "Iqdb 3D")
+        setattr(Sauce, 'another', saucer_Iqdb3d(name).raw)
+    
+
 
 def saucer_sauceNao(name):
-    sauce = saucer.from_url(name)
+    b = open("API_sauceNao")
+    saucer = SauceNAO(b.readline())
+    sauce = saucer.search(name)
+    return sauce
+
+
+def saucer_TraceMoe(name):
+    saucer = TraceMoe()
+    sauce = saucer.search(name)
+    return sauce
+
+
+def saucer_Ascii2D(name):
+    saucer = Ascii2D()
+    sauce = saucer.search(name)
+    return sauce
+
+
+def saucer_Iqdb(name):
+    saucer = Iqdb()
+    sauce = saucer.search(name)
+    return sauce
+
+
+def saucer_Iqdb3d(name):
+    saucer = Iqdb()
+    sauce = saucer.search_3d(name)
     return sauce
