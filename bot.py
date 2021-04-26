3db39a5c0d436a09cc660bee2206237e0440b7f8
lleans <ahmadunnail@gmail.com>
2021-04-26 10:21:12 +0700

    stable release

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ import random
 import traceback
 import saucer
 
+from aiohttp import ClientSession
 from colorthief import ColorThief
-from urllib.request import urlopen, Request
 
 
 class MaidHayasaka(discord.Client):
@@ -25,63 +25,37 @@ class MaidHayasaka(discord.Client):
         self.video = re.compile(
             r'(.*avi)|(.*m4v)|(.*mpeg)|(.*mpg)|(.*webm)|(.*mp4)')
 
-        # Discord embed system message
-        url_eg = {
-            "url_eg": '`"https: //i.imgur.com/1Czc ..."`',
-            "override_eg": '`,https: //i.imgur.com/F7ed ...,`'
-        }
-        self.help_message = {
-            'title': "Help Commands",
-            'desc': "So this is my first time with you\nso please be gentle with me....\n\n\nAnyway here the command list",
-            'image': "https://i.imgur.com/38MpUXM.gif",
-            'field_title': ['Basic Command', 'Override Command'],
-            'color': [255, 224, 99],
-            'field_value': [f"If you find a picture or video, just right click and click copy link, then enter it like the command above\n\ne.g {url_eg['url_eg']}", f"For this case, when you found some anime screenshot or gif anything that you think it is anime, you can use this command to search for it\n\ne.g {url_eg['override_eg']}"]
-        }
-        self.error_message = {
-            'title': "Whoopsss.....",
-            'desc': "Looks like i couldn't find the sauce or there is something went wrong, maybe API down or anything else, or even god doesn't like it\n\n**What should i do ?**\nYou can use these website to reverse image manually\n",
-            'image': 'https://i.imgur.com/foNFxKu.gif',
-            'field_title': ['Multi Service', 'Manga, anime & art', 'Everything'],
-            'color': [255, 96, 56],
-            'field_value': [f"[Iqdb]({'https://iqdb.org/'})\n[ImgOps]({'https://imgops.com/'})", f"[SauceNao]({'https://saucenao.com/'})\n[Ascii2D]({'https://ascii2d.net/'})\n[TraceMoe]({'https://trace.moe/'})", f"[Google Images]({'https://images.google.com/'})\n[TinEye]({'https://tineye.com/'})\n[Yandex]({'https://yandex.com/images/'})"]
-        }
-        self.wait_message = {
-            'title': "Looking for the sauce....",
-            'desc': ["nice cock bruh",
-                     "are you horny ?",
-                     "just another degenerate weeb doing the stuff",
-                     "Peter Piper picked a peck of pickled peppers\nA peck of pickled peppers Peter Piper picked\nIf Peter Piper picked a peck of pickled peppers\nWhere’s the peck of pickled peppers Peter Piper picked?",
-                     "Hi cunt",
-                     "The coconut nut is a giant nut\nIf you eat too much you'll get very fat\nNow the coconut nut is a big-big nut\nBut it's delicious nut is not a nut\nIt's the coco fruit (it's the coco fruit)\nOf the coco tree (of the coco tree)\nFrom the coco palm family"],
-            'image': 'https://i.imgur.com/GE8uaS4.gif',
-            'field_title': None,
-            'color': [255, 201, 107],
-            'field_value': None
-        }
         self.kadal = kadal.Klient()
         self.sauce = saucer.Sauce()
         print("start")
 
-    def embed_builder(self, message, type, error=None):
-        if type == "wait":
-            data = self.wait_message
-        elif type == "help":
-            data = self.help_message
-        else:
-            data = self.error_message
+    @staticmethod
+    def help(message):
+        e = discord.Embed(title="Help Commands", description="So this is my first time with you\nso please be gentle with me....\n\n\nAnyway here the command list", color=discord.Color.from_rgb(255, 224, 99)).set_thumbnail(url="https://i.imgur.com/38MpUXM.gif").add_field(name="Basic Command", value='If you find a picture or video, just right click and click copy link, then enter it like the command above\n\ne.g `"https: //i.imgur.com/1Czc ..."`').add_field(
+            name="Override Command", value='For this case, when you found some anime screenshot or gif anything that you think it is anime, you can use this command to search for it\n\ne.g `,https: //i.imgur.com/F7ed ...,`').set_author(name=maid.user.name, icon_url=maid.user.avatar_url).set_footer(text=f"© {maid.user.name} | {message.created_at.strftime('%x')}")
+        return e
 
-        e = discord.Embed(title=data['title'], description=data['desc'] if type != "wait" else random.choice(data['desc']), color=discord.Color.from_rgb(
-            data['color'][0], data['color'][1], data['color'][2])).set_author(name=maid.user.name, icon_url=maid.user.avatar_url).set_thumbnail(url=data['image'])
-        if data['field_title'] is not None:
-            for index, a in enumerate(data['field_title']):
-                e.add_field(
-                    name=a, value=data['field_value'][index], inline=True)
-        e.set_footer(text=f"© {maid.user.name} | {message.created_at.strftime('%x')}" if error ==
-                     None else f"© {maid.user.name} | {message.created_at.strftime('%x')} | {error}")
+    @staticmethod
+    def error(message, error):
+        e = discord.Embed(title="Whoopsss.....", description="Looks like i couldn't find the sauce or there is something went wrong, maybe API down or even god doesn't like it\n\n**What should i do ?**\nYou can use these website to reverse image manually\n", color=discord.Color.from_rgb(255, 96, 56)).set_thumbnail(url="https://i.imgur.com/foNFxKu.gif").add_field(name='Multi Service', value=f"[Iqdb]({'https://iqdb.org/'})\n[ImgOps]({'https://imgops.com/'})").add_field(
+            name='Manga, anime & art', value=f"[SauceNao]({'https://saucenao.com/'})\n[Ascii2D]({'https://ascii2d.net/'})\n[TraceMoe]({'https://trace.moe/'})").add_field(name='Everything', value=f"[Google Images]({'https://images.google.com/'})\n[TinEye]({'https://tineye.com/'})\n[Yandex]({'https://yandex.com/images/'})").set_author(name=maid.user.name, icon_url=maid.user.avatar_url).set_footer(text=f"© {maid.user.name} | {message.created_at.strftime('%x')} | {error}")
+        return e
+
+    @staticmethod
+    def wait(message):
+        rand = random.choice(["nice cock bruh",
+                              "are you horny ?",
+                              "just another degenerate weeb doing the stuff",
+                              "Peter Piper picked a peck of pickled peppers\nA peck of pickled peppers Peter Piper picked\nIf Peter Piper picked a peck of pickled peppers\nWhere’s the peck of pickled peppers Peter Piper picked?",
+                              "Hi cunt",
+                              "The coconut nut is a giant nut\nIf you eat too much you'll get very fat\nNow the coconut nut is a big-big nut\nBut it's delicious nut is not a nut\nIt's the coco fruit (it's the coco fruit)\nOf the coco tree (of the coco tree)\nFrom the coco palm family"])
+        e = discord.Embed(title="Looking for the sauce....", description=rand, color=discord.Color.from_rgb(255, 201, 107)).set_thumbnail(
+            url="https://i.imgur.com/GE8uaS4.gif").set_author(name=maid.user.name, icon_url=maid.user.avatar_url).set_footer(text=f"© {maid.user.name} | {message.created_at.strftime('%x')}")
         return e
 
     async def format_embed(self, sauce, *, type, message, original, anilist=False):
+        footer = f"© {maid.user.name} | {message.created_at.strftime('%x')}"
+        # Anilist
         try:
             media = await type(sauce['title'], popularity=True, allow_adult=True)
             if media.description is not None:
@@ -90,48 +64,44 @@ class MaidHayasaka(discord.Client):
                 hex_color = media.cover_color or random.choice(
                     self.RANDOM_COLOR)
                 color_anilist = int(hex_color.lstrip('#'), 16)
-                desc = f"Likely **{str(sauce['similiar'])}**\n\n***{', '.join(media.genres)}***\n"
-                desc += media.description[:256 - len(desc)] + f"... [(more)]({media.site_url})\n\nAnother Results: \n"
-                desc = desc.replace("<br>", "").replace("<i>", "").replace("<b>", "")
+                desc = f"Likely **{sauce['similiar']}%**\n\n***{', '.join(media.genres)}***\n"
+                anilist_description = media.description.replace("<br>", "").replace("<i>", "").replace("<b>", "").replace("</br>", "").replace("/<i>", "").replace("</b>", "")
+                desc += anilist_description[:256 - len(desc)] + f"... [(more)]({media.site_url})\n\nAnother Results: \n"
             else:
                 desc = "\nAnother Results: \n"
         except:
-            desc = "Likely **" + str(sauce['similiar']) + \
-                f"%**\n\nAnother Results: \n"
-        # find title at anilist, if exist
+            desc = f"Likely **{sauce['similiar']}%**\n\nAnother Results: \n"
 
+        # Another Resulta
         for x in range(3):
             try:
                 desc += f"** • [{sauce['another_titles'][x][:150 - len(sauce['another_titles'][x])]} ...]({sauce['another_urls'][x]})**\n"
             except:
                 continue
-        # give another results
-        footer = f"© {maid.user.name} | {message.created_at.strftime('%x')}"
+
+        # Embed
         if anilist == True:
             e = discord.Embed(title=sauce['title'],
                               description=desc, color=color_anilist)
             e.set_image(url=thumbnail_anilist)
-            e.set_thumbnail(url=original)
         else:
             try:
-                req = Request(sauce['thumbnail'], headers={
-                              'User-Agent': 'Mozilla/5.0'})
-                f = io.BytesIO(urlopen(req).read())
-                e = discord.Embed(title=sauce.title,
-                                  description=desc, color=int(('%02x%02x%02x' % ColorThief(f).get_color(quality=1)).lstrip('#'), 16))
-                e.set_thumbnail(url=original)
-                e.set_image(url=sauce['thumbnail'])
+                async with ClientSession() as Session:
+                    async with Session.get(sauce['thumbnail'], allow_redirects=True, headers={'User-Agent': 'Mozilla/5.0'}) as data:
+                        f = io.BytesIO(await data.read())
+                        e = discord.Embed(title=sauce['title'], description=desc, color=int(('%02x%02x%02x' % ColorThief(f).get_color(quality=1)).lstrip('#'), 16))
+                        e.set_image(url=sauce['thumbnail'])
             except:
                 if sauce['thumbnail'][:4] == 'http':
                     desc += f"\nLooks like the image doesn't show up, [click here]({sauce['thumbnail']}) to open it"
                 else:
                     desc += "\nLooks like there is no thumbnail for this, try click one of the results to open it"
-                e = discord.Embed(title=sauce['title'],
-                                  description=desc, color=int(random.choice(self.RANDOM_COLOR).lstrip('#'), 16))
-                e.set_thumbnail(url=original)
+                e = discord.Embed(title=sauce['title'], description=desc, color=int(
+                    random.choice(self.RANDOM_COLOR).lstrip('#'), 16))
                 e.set_image(url="https://i.imgur.com/1CzcRfk.gif")
                 footer += " | Image not found 404"
         e.url = sauce['url']
+        e.set_thumbnail(url=original)
         e.set_author(name=sauce['source'][1], icon_url=sauce['source'][0])
         e.set_footer(icon_url=message.author.avatar_url, text=footer)
         return e
@@ -141,30 +111,30 @@ class MaidHayasaka(discord.Client):
         await maid.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"@{maid.user.name}"))
 
     async def search(self, name, message, video):
-            temp = await message.channel.send(embed=self.embed_builder(message, "wait"))
-            if video or self.video.findall(name):
-                try:
-                    sauce = await self.sauce.sauce_anime(name=name)
-                    embed = await self.format_embed(sauce, type=self.kadal.search_anime, message=message, original=name)
-                    await temp.delete()
-                    if self.video.findall(name):
-                        await message.channel.send(name)
-                    await message.channel.send(embed=embed)
-                    await message.channel.send(sauce['url'])
-                except Exception as catch:
-                    await temp.delete()
-                    traceback.print_exc()
-                    await message.channel.send(embed=self.embed_builder(message, "error", catch))
-            else:
-                try:
-                    sauce = await self.sauce.sauce_image(name=name)
-                    embed = await self.format_embed(sauce, type=self.kadal.search_manga, message=message, original=name)
-                    await temp.delete()
-                    await message.channel.send(embed=embed)
-                except Exception as catch:
-                    await temp.delete()
-                    traceback.print_exc()
-                    await message.channel.send(embed=self.embed_builder(message, "error", catch))
+        temp = await message.channel.send(embed=self.wait(message))
+        if video or self.video.findall(name):
+            try:
+                sauce = await self.sauce.sauce_anime(name=name)
+                embed = await self.format_embed(sauce, type=self.kadal.search_anime, message=message, original=name)
+                await temp.delete()
+                if self.video.findall(name):
+                    await message.channel.send(name)
+                await message.channel.send(embed=embed)
+                await message.channel.send(sauce['url'])
+            except Exception as catch:
+                await temp.delete()
+                traceback.print_exc()
+                await message.channel.send(embed=self.error(message, catch))
+        else:
+            try:
+                sauce = await self.sauce.sauce_image(name=name)
+                embed = await self.format_embed(sauce, type=self.kadal.search_manga, message=message, original=name)
+                await temp.delete()
+                await message.channel.send(embed=embed)
+            except Exception as catch:
+                await temp.delete()
+                traceback.print_exc()
+                await message.channel.send(embed=self.error(message, catch))
 
     async def filter(self, message, regex, method):
         m = regex.findall(message.clean_content)
@@ -175,7 +145,7 @@ class MaidHayasaka(discord.Client):
                     if name[:4] == "http":
                         await self.search(name, message, method)
             elif m_clean[0] == "help":
-                await message.channel.send(embed=self.embed_builder(message, "help"))
+                await message.channel.send(embed=self.help(message))
             else:
                 if m_clean[0][:4] == "http":
                     await self.search(m_clean[0], message, method)
@@ -183,8 +153,8 @@ class MaidHayasaka(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if maid.user in message.mentions:
-            await message.channel.send(embed=self.embed_builder(message, "help"))
+        if maid.user in message.mentions and message.clean_content == f"@{maid.user.name}":
+            await message.channel.send(embed=self.help(message))
         # ignore her own message
         for type, regex in self.regex_command.items():
             method = True if type == "video" else False
--- saucer.py
+++ saucer.py
@@ -1,185 +1,153 @@
 import re
 import asyncio
-from PicImageSearch import SauceNAO, TraceMoe, Ascii2D, Iqdb, Google
+import PicImageSearch
 
 
 class Sauce:
     def __init__(self):
         super().__init__()
+        self.client = PicImageSearch.Search()
 
-    @staticmethod
-    def arrange_data(data, similiar, source):
+    async def sauce_anime(self, name):
+        tMoetask = await self.client.tracemoe(url=name)
+        tMoe = tMoetask.raw
+        similiar = tMoe[0].similarity
         res = {
+            'title': tMoe[0].title_english,
             'similiar': similiar,
-            'source': source,
+            'source': ["https://i.imgur.com/aXJEPmD.png", "TraceMoe"],
+            'thumbnail': tMoe[0].thumbnail,
+            'url': tMoe[0].video_thumbnail,
             'another_titles': list(),
             'another_urls': list()
         }
-        if source[1] == "TraceMoe":
-            res.update({
-                'title': data[0].title_english,
-                'thumbnail': data[0].thumbnail,
-                'url': data[0].video_thumbnail
-            })
-            for x in range(1, len(data)):
-                try:
-                    res['another_titles'].append(data[x].title)
-                    res['another_urls'].append(data[x].video_thumbnail)
-                except:
-                    continue
-        elif source[1] == "Google":
-            res.update({
-                'title': data[2].titles[0],
-                'thumbnail': data[2].thumbnail[0],
-                'url': data[2].urls[0]
-            })
-            for x in range(3, len(data)):
-                try:
-                    res['another_titles'].append(data[x].titles[0])
-                    res['another_urls'].append(data[x].urls[0])
-                except:
-                    continue
-        elif source[1] == "Ascii2d":
-            res.update({
-                'title': data[1].titles[0],
-                'thumbnail': data[1].thumbnail[0],
-                'url': data[1].urls[0]
-            })
-            for x in range(2, len(data)):
-                try:
-                    res['another_titles'].append(data[x].titles[0])
-                    res['another_urls'].append(data[x].urls[0])
-                except:
-                    continue
-        elif source[1] == "SauceNao":
-            res.update({
-                'title': data[0].title,
-                'thumbnail': data[0].thumbnail,
-                'url': data[0].url
-            })
-            for x in range(1, len(data)):
-                try:
-                    res['another_titles'].append(data[x].title)
-                    res['another_urls'].append(data[x].url)
-                except:
-                    continue
-        elif source[1] == "Iqdb" or source[1] == "Iqdb 3D":
-            res.update({
-                'title': data[0].title[:180 - len(data[0].title)] + "...",
-                'thumbnail': data[0].thumbnail,
-                'url': data[0].url if data[0].url[:4] == "http" else f"https:{data[0].url}"
-            })
-            for x in range(1, len(data)):
-                try:
-                    res['another_titles'].append(
-                        data[x].title[:180 - len(data[0].title)] + "...")
-                    res['another_urls'].append(
-                        data[x].url if data[0].url[:4] == "http" else f"https:{data[0].url}")
-                except:
-                    continue
-        
+        for x in range(1, len(tMoe)):
+            try:
+                res['another_titles'].append(tMoe[x].title)
+                res['another_urls'].append(tMoe[x].video_thumbnail)
+            except:
+                continue
         return res
 
-    @staticmethod
-    async def saucer_sauceNao(name):
-        b = open("API_sauceNao")
-        saucer = SauceNAO(b.readline())
-        sauce = saucer.search(name)
-        return sauce
-
-    @staticmethod
-    async def saucer_TraceMoe(name):
-        saucer = TraceMoe()
-        sauce = saucer.search(name)
-        return sauce
-
-    @staticmethod
-    async def saucer_Ascii2D(name):
-        saucer = Ascii2D()
-        sauce = saucer.search(name)
-        return sauce
-
-    @staticmethod
-    async def saucer_Iqdb(name):
-        saucer = Iqdb()
-        try:
-            sauce = saucer.search(name)
-            return sauce
-        except:
-            return None
-
-    @staticmethod
-    async def saucer_Iqdb3d(name):
-        saucer = Iqdb()
-        try:
-            sauce = saucer.search_3d(name)
-            return sauce
-        except:
-            return None
-
-    @staticmethod
-    async def saucer_Google(name):
-        saucer = Google()
-        sauce = saucer.search(name)
-        return sauce
-
-    async def sauce_anime(self, name):
-        tMoetask = await asyncio.create_task(self.saucer_TraceMoe(name))
-        tMoe = tMoetask.raw
-        similiar = float("{:.2f}".format(tMoe[0].similarity * 100))
-
-        return self.arrange_data(
-            tMoe, similiar, ["https://i.imgur.com/aXJEPmD.png", "TraceMoe"])
-
     async def sauce_image(self, name):
-        REGEXIQ = re.compile("[0-9]+")
         # Request
-        Googletask, sNaotask, A2dtask, Iqtask, Iq3dtask = await asyncio.gather(self.saucer_Google(name), self.saucer_sauceNao(name), self.saucer_Ascii2D(name), self.saucer_Iqdb(name), self.saucer_Iqdb3d(name))
+        Googletask, sNaotask, A2dtask, Iqtask, Iq3dtask = await asyncio.gather(self.client.google(url=name), self.client.saucenao(url=name, api_key=open("API_sauceNao").readline()), self.client.ascii2d(url=name), self.client.iqdb(url=name), self.client.iqdb_3d(url=name))
         Google, sNao, A2d = Googletask.raw, sNaotask.raw, A2dtask.raw
 
         try:
             Iq = Iqtask.raw
-            Iqs = REGEXIQ.search(Iq[0].similarity)
-            Iqsimilar = float(Iqs.group(0))
+            Iqsimilar = Iq[0].similarity
         except:
             Iqsimilar = float(0)
 
         try:
             Iq3d = Iq3dtask.raw
-            Iq3D = REGEXIQ.search(Iq3d[0].similarity)
-            Iq3dsimilar = float(Iq3D.group(0))
+            Iq3dsimilar = Iq3d[0].similarity
         except:
             Iq3dsimilar = float(0)
 
         try:
-            Googledata = Google[2].titles[0]
-            self.thumbnail = Google[2].thumbnail[0]
-        except IndexError:
+            Googledata = Google[2].title
+            if Google[2].thumbnail == "":
+                Googledata = None
+        except:
             Googledata = None
 
-        sNaosimilar = float(sNao[0].similarity)
+        sNaosimilar = sNao[0].similarity
 
         if sNaosimilar <= 80 and Iqsimilar <= 80 and Iq3dsimilar <= 80 and Googledata is not None:
             # Google
-            return self.arrange_data(
-                Google, None, ["https://i.imgur.com/Z9OLjXS.png", "Google"])
+            res = {
+                'title': Google[2].title,
+                'thumbnail': Google[2].thumbnail,
+                'similiar': None,
+                'source': ["https://i.imgur.com/Z9OLjXS.png", "Google"],
+                'url': Google[2].url,
+                'another_titles': list(),
+                'another_urls': list()
+            }
+            for x in range(3, len(Google)):
+                try:
+                    res['another_titles'].append(Google[x].title)
+                    res['another_urls'].append(Google[x].url)
+                except:
+                    continue
+            return res
 
         elif sNaosimilar <= 80 and Iqsimilar <= 80 and Iq3dsimilar <= 80 and Googledata is None:
             # Ascii2d
-            return self.arrange_data(
-                A2d, None, ["https://i.imgur.com/BA7hWTm.png", "Ascii2d"])
+            res = {
+                'title': A2d[1].title,
+                'thumbnail': A2d[1].thumbnail,
+                'similiar': None,
+                'source': ["https://i.imgur.com/BA7hWTm.png", "Ascii2d"],
+                'url': A2d[1].url,
+                'another_titles': list(),
+                'another_urls': list()
+            }
+            for x in range(2, len(A2d)):
+                try:
+                    res['another_titles'].append(A2d[x].title)
+                    res['another_urls'].append(A2d[x].url)
+                except:
+                    continue
+            return res
 
         elif sNaosimilar >= Iqsimilar and sNaosimilar >= Iq3dsimilar:
             # SauceNao
-            return self.arrange_data(sNao, sNaosimilar, [
-                              "https://i.imgur.com/FhsgOiv.png", "SauceNao"])
+            res = {
+                'title': sNao[0].title,
+                'thumbnail': sNao[0].thumbnail,
+                'similiar': sNaosimilar,
+                'source': ["https://i.imgur.com/FhsgOiv.png", "SauceNao"],
+                'url': sNao[0].url,
+                'another_titles': list(),
+                'another_urls': list()
+            }
+            for x in range(1, len(sNao)):
+                try:
+                    res['another_titles'].append(sNao[x].title)
+                    res['another_urls'].append(sNao[x].url)
+                except:
+                    continue
+            return res
 
         elif Iqsimilar >= sNaosimilar and Iqsimilar >= Iq3dsimilar:
             # Iqdb
-            return self.arrange_data(
-                Iq, Iqsimilar, ["https://i.imgur.com/r3kJwPF.png", "Iqdb"])
+            res = {
+                'title': Iq[0].title[:180 - len(Iq[0].title)] + "...",
+                'thumbnail': Iq[0].thumbnail,
+                'similiar': Iqsimilar,
+                'source': ["https://i.imgur.com/r3kJwPF.png", "Iqdb"],
+                'url': Iq[0].url,
+                'another_titles': list(),
+                'another_urls': list()
+            }
+            for x in range(1, len(Iq)):
+                try:
+                    res['another_titles'].append(
+                        Iq[x].title[:180 - len(Iq[0].title)] + "...")
+                    res['another_urls'].append(Iq[x].url)
+                except:
+                    continue
+            return res
 
         elif Iq3dsimilar >= sNaosimilar and Iq3dsimilar >= Iqsimilar:
             # Iqdb 3D
-            return self.arrange_data(Iq3d, Iq3dsimilar, [
-                              "https://i.imgur.com/r3kJwPF.png" "Iqdb 3D"])
+            res = {
+                'title': Iq3d[0].title[:180 - len(Iq3d[0].title)] + "...",
+                'thumbnail': Iq3d[0].thumbnail,
+                'similiar': Iq3dsimilar,
+                'source': ["https://i.imgur.com/r3kJwPF.png", "Iqdb 3D"],
+                'url': Iq3d[0].url,
+                'another_titles': list(),
+                'another_urls': list()
+            }
+            for x in range(1, len(Iq3d)):
+                try:
+                    res['another_titles'].append(
+                        Iq3d[x].title[:180 - len(Iq3d[0].title)] + "...")
+                    res['another_urls'].append(Iq3d[x].url)
+                except:
+                    continue
+            return res
